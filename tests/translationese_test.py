e38001d69a1fdab51065651aff6335dc4651f9a7
Gal Star <gal.star3051@gmail.com>
2012-11-27 20:51:30 +0200

    adding unitest for mean_word_length

--- tests/translationese_test.py
+++ tests/translationese_test.py
@@ -6,3 +6,11 @@ class TypeTokenRatioTest(unittest.TestCase):
         s = """Hello hello world world"""
 
         self.assertEqual(0.5, translationese.type_token_ratio(s))
+
+class MeanWordLength(unittest.TestCase):
+    def test_simple(self):
+        s1 = """hello there luzky!"""
+        s2 = """is good-will"""
+
+        self.assertEqual(5, translationese.mean_word_length(s1))
+        self.assertEqual(5.5, translationese.mean_word_length(s2))
