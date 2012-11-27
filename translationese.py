615b96bbdafd5c5aefa58cd810d2ddeb44ec191d
Gal Star <gal.star3051@gmail.com>
2012-11-27 20:34:46 +0200

    adding mean_word_length attribute.

--- translationese.py
+++ translationese.py
@@ -6,3 +6,11 @@ def average_sentence_length(s):
     sentences = nltk.sent_tokenize(s)
 
     return float(sum([sentence_length(x) for x in sentences])) / len(sentences)
+
+def mean_word_length(s):
+    sentences = nltk.sent_tokenize(s)
+    words = []
+    for sentence in sentences: words += nltk.word_tokenize(sentence)
+
+    real_words = [w for w in words if w[0].isalpha()]
+    return float(sum([len(w) for w in real_words])) / len(real_words)
