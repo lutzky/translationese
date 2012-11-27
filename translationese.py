f3886af3fd02fa82d2ca37f49aab33f2259fe3cd
Ohad Lutzky <ohad@lutzky.net>
2012-11-27 19:53:24 +0200

    Add type-token ratio and tests

--- translationese.py
+++ translationese.py
@@ -6,3 +6,18 @@ def average_sentence_length(s):
     sentences = nltk.sent_tokenize(s)
 
     return float(sum([sentence_length(x) for x in sentences])) / len(sentences)
+
+def type_token_ratio(s):
+    # We tokenize into sentences and then into words due to a warning
+    # in the NLTK API doc to only word_tokenize single sentences.
+
+    sentences = nltk.sent_tokenize(s)
+    
+    words = []
+
+    for sentence in sentences:
+        words += [ w.upper() for w in nltk.word_tokenize(sentence) ]
+
+    words_set = set(words)
+
+    return len(set(words)) / float(len(words))
