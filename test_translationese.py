7d1060add14b44e1c5cb5e766769a28875d59d8a
Ohad Lutzky <ohad@lutzky.net>
2012-12-05 01:02:15 +0200

    Formatting changes

--- test_translationese.py
+++ test_translationese.py
@@ -8,16 +8,16 @@ class Histogram(unittest.TestCase):
         a = translationese.Analysis(sentence)
         self.assertEquals(
                           {
-                           "how": 1/14.0,
-                           "much": 1/14.0,
-                           "wood": 2/14.0,
-                           "would": 1/14.0,
-                           "a": 2/14.0,
-                           "woodchuck": 2/14.0,
-                           "if": 1/14.0,
-                           "chuck": 2/14.0,
-                           "could": 1/14.0,
-                           "?": 1/14.0,
+                           "how": 1 / 14.0,
+                           "much": 1 / 14.0,
+                           "wood": 2 / 14.0,
+                           "would": 1 / 14.0,
+                           "a": 2 / 14.0,
+                           "woodchuck": 2 / 14.0,
+                           "if": 1 / 14.0,
+                           "chuck": 2 / 14.0,
+                           "could": 1 / 14.0,
+                           "?": 1 / 14.0,
                            },
                           a.histogram_normalized()
                           )
