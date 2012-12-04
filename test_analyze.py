9a6b032e0e0f990680ddb658e523040149997841
Ohad Lutzky <ohad@lutzky.net>
2012-12-05 01:02:23 +0200

    Add tests for analyze.py

---
+++ test_analyze.py
@@ -0,0 +1,39 @@
+'''
+Created on Dec 5, 2012
+
+@author: ohad
+'''
+import unittest
+
+import analyze
+import StringIO
+import translationese.lexical_variety
+import os.path
+
+class TestAnalyze(unittest.TestCase):
+    def setUp(self):
+        tests_dir = os.path.join(os.path.dirname(__file__), "tests")
+        self.o_dir = os.path.join(tests_dir, "o")
+        self.t_dir = os.path.join(tests_dir, "t")
+
+    def assertResultForModule(self, module, expected):
+        s = StringIO.StringIO()
+        analyze.main(module, self.o_dir, self.t_dir, s)
+        self.assertMultiLineEqual(expected, s.getvalue())
+        pass
+
+    def testWithLexicalVariety(self):
+        self.assertResultForModule(translationese.lexical_variety, \
+                                   lexical_variety_result)
+
+lexical_variety_result = """\
+@relation translationese
+@attribute class { T, O }
+@attribute type_token_ratio numeric
+
+@data
+O,0.714285714286
+O,0.714285714286
+T,0.8
+T,1.0
+"""
