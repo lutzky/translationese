4f3a34a99f85cf49eacf14ffa7839b818e7fcb69
Ohad Lutzky <ohad@lutzky.net>
2012-12-05 00:05:29 +0200

    Add function words

---
+++ translationese/test_function_words.py
@@ -0,0 +1,20 @@
+import unittest
+import translationese
+
+sentence = "The quick brown fox jumped over the lazy dog."
+
+class TestFunctionWords(unittest.TestCase):
+    def setUp(self):
+        self.analysis = translationese.Analysis(sentence)
+        self.double_analysis = translationese.Analysis(sentence + \
+                                                       " " + sentence)
+        self.result = translationese.function_words.quantify(self.analysis)
+        self.double_result = translationese.function_words.quantify(self.double_analysis)
+
+    def testThe(self):
+        self.assertEquals(2 / 10.0, self.result["the"])
+        self.assertEquals(2 / 10.0, self.double_result["the"])
+
+    def testOver(self):
+        self.assertEquals(1 / 10.0, self.result["over"])
+        self.assertEquals(1 / 10.0, self.double_result["over"])
