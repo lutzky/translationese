4a96ee8b144b37b95950cc54684415e21d2bd8dc
Ohad Lutzky <ohad@lutzky.net>
2012-12-12 00:54:58 +0200

    Better tests for contractions

--- tests/test_contractions.py
+++ tests/test_contractions.py
@@ -5,15 +5,22 @@ Created on Dec 11, 2012
 '''
 import unittest
 from translationese import contractions
-import translationese
+from translationese import Analysis
 
 class TestContractions(unittest.TestCase):
     def testContractions(self):
-        a = translationese.Analysis("I'm certain he said I am "
-                                    "later than I am usually. Let's go.")
+        a = Analysis("I'm certain he said I am later than I am, "
+                     "let's go.")
         result = contractions.quantify(a)
         self.assertAlmostEqual(0.5, result["i'm"])
         self.assertAlmostEqual(1.0, result["let's"])
+        self.assertEqual(0.0, result["didn't"])
+
+    def testMultiContractions(self):
+        a = Analysis("What's the difference between what is shown "
+                     "now and what has been shown before?")
+        result = contractions.quantify(a)
+        self.assertAlmostEqual(0.5, result["what's"])
 
 if __name__ == "__main__":
     unittest.main()
