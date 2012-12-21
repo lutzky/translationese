b375eef931bee6f4c61c35cbb187d2c58818a288
Ohad Lutzky <ohad@lutzky.net>
2012-12-21 09:59:48 +0200

    Properly implement variants for lexical density
    Relevant to #2.
    
    This changes test_analyze as it now runs an analysis of a module with
    variants.

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -17,25 +17,23 @@ class TestAnalyze(unittest.TestCase):
         self.o_dir = os.path.join(tests_dir, "o")
         self.t_dir = os.path.join(tests_dir, "t")
 
-    def assertResultForModule(self, module, expected):
+    def assertResultForModule(self, module, variant, expected):
         s = StringIO.StringIO()
-        analyze.main(module, self.o_dir, self.t_dir, s)
+        analyze.main(module, self.o_dir, self.t_dir, s, variant)
         self.assertMultiLineEqual(expected, s.getvalue())
 
     def testWithLexicalVariety(self):
-        self.assertResultForModule(translationese.lexical_variety, \
+        self.assertResultForModule(translationese.lexical_variety, 1, \
                                    lexical_variety_result)
 
 lexical_variety_result = """\
 @relation translationese
-@attribute 'type_token_ratio' numeric
-@attribute 'log_type_token_ratio' numeric
-@attribute 'unique_type_token_ratio' numeric
+@attribute 'TTR2' numeric
 @attribute class { T, O }
 
 @data
-4.28571428571,5.23501721729,659.764332404,O
-4.28571428571,4.96252485208,486.477537264,O
-6.0,6.0,inf,T
-4.8,5.16811869688,643.775164974,T
+5.23501721729,O
+4.96252485208,O
+6.0,T
+5.16811869688,T
 """
