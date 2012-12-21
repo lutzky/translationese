6e4aef6604ec7c089e6cfa900f9b4fc02f890cde
Ohad Lutzky <ohad@lutzky.net>
2012-12-21 15:54:04 +0200

    Implement punctuation variants
    Closes #26

--- tests/test_punctuation.py
+++ tests/test_punctuation.py
@@ -9,13 +9,37 @@ from translationese import Analysis
 from translationese import punctuation
 
 class TestPunctuation(unittest.TestCase):
-    def testSimple(self):
-        a = Analysis("""How now, "brown" 'cow'?""")
-        expected_normalized = {
-                               '?': 1.0 / 2000,
-                               "'": 2.0 / 2000,
-                               '"': 2.0 / 2000
-                               }
+    def setUp(self):
+        self.sentence = """Punctuation: Small, easily-overlooked symbols, which can """ \
+                        """completely change the "meaning" of a sentence."""
+        self.analysis = Analysis(self.sentence)
 
-        self.assertDictContainsSubset(expected_normalized,
-                                      punctuation.quantify(a))
+    def testVariant1(self):
+        result = punctuation.quantify_variant(self.analysis, 0)
+        expected = {
+                    ':': 1 / 2000.0,
+                    ',': 2 / 2000.0,
+                    '.': 1 / 2000.0,
+                    '"': 2 / 2000.0,
+                    }
+        self.assertDictContainsSubset(expected, result)
+
+    def testVariant2(self):
+        result = punctuation.quantify_variant(self.analysis, 1)
+        expected = {
+                    ':': 4 * 1 / 19.0,
+                    ',': 4 * 2 / 19.0,
+                    '.': 4 * 1 / 19.0,
+                    '"': 4 * 2 / 19.0,
+                    }
+        self.assertDictContainsSubset(expected, result)
+
+    def testVariant3(self):
+        result = punctuation.quantify_variant(self.analysis, 2)
+        expected = {
+                    ':': 4 * 1 / 7.0,
+                    ',': 4 * 2 / 7.0,
+                    '.': 4 * 1 / 7.0,
+                    '"': 4 * 2 / 7.0,
+                    }
+        self.assertDictContainsSubset(expected, result)
