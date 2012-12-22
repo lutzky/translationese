c4b87841ce49d03cd3ffca67d65df51cbf248148
Gal Star <gal.star3051@gmail.com>
2012-12-22 13:34:25 +0200

    Fixing the still un-fixed build of lexical density

--- tests/test_lexical_density.py
+++ tests/test_lexical_density.py
@@ -7,12 +7,10 @@ class LexcialDensityTest(unittest.TestCase):
         a1 = translationese.Analysis("""Hello hello world world""")
         a2 = translationese.Analysis("""I'm certain he said I am later than I am, 
                                      let's go""")
-        quantifier1 = lexical_density.quantify(a1)
-        self.assertEqual(0.0, quantifier1)
-        quantifier2 = lexical_density.quantify(a2)
-        self.assertEqual((7.0)/(15.0), quantifier2)
-        
         a3 = translationese.Analysis("""we will go tomorrow to see a movie""")
         a4 = translationese.Analysis("""What about punctuation?""")
-        self.assertEqual(0.5, a3.lexical_density())
-        self.assertEqual(3.0/4.0, a4.lexical_density())
+        
+        self.assertEqual(0.0, lexical_density.quantify(a1))
+        self.assertEqual((7.0)/(15.0), lexical_density.quantify(a2))
+        self.assertEqual(0.5, lexical_density.quantify(a3))
+        self.assertEqual(3.0/4.0, lexical_density.quantify(a4))
