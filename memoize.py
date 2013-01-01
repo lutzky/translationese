bc594392debc8b61e042b69d35d20dcce487b2f9
Ohad Lutzky <ohad@lutzky.net>
2013-01-02 01:32:02 +0200

    memoize: Add doctest
    Also properly @wrap...

--- memoize.py
+++ memoize.py
@@ -1,7 +1,25 @@
+"""Usable as a wrapper to a method, caches execution results.
+
+>>> class K:
+...     def __init__(self):
+...         self.x = 5
+...     @memoize
+...     def m(self):
+...         return self.x * 2
+>>> k = K()
+>>> k.m()
+10
+>>> k.x = 6
+>>> k.m() # Will not recompute
+10
+>>> import pickle
+>>> s = pickle.dumps(k) # resulting objects can be pickled!
+"""
+
 from functools import wraps
 
 def memoize(func):
-    wraps(func)
+    @wraps(func)
     def wrapper(self):
         if not hasattr(self, "__memoize_cache"):
             self.__memoize_cache = {}
