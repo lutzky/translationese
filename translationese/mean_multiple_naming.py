048c2e5db2121b911f0196454ec10a07d1f0b059
Gal Star <gal.star3051@gmail.com>
2013-01-15 22:51:22 +0200

    Adding mean multiple naming attibute

---
+++ translationese/mean_multiple_naming.py
@@ -0,0 +1,30 @@
+"""\
+Implementation of Mean multiple naming hypothesis.
+
+Origin: On the Features of Translationese, VV, NO & SW
+        4.2 Expplicitation, Mean multiple naming
+"""
+from nltk.tag import pos_tag
+
+__author__ = "Gal Star"
+__email__ = "gal.star3051@gmail.com"
+
+import nltk
+
+attributes = [ "mean_multiple_naming" ]
+
+def is_proper_noun(pos):
+    return (pos == "NNP")
+
+def tokens_without_punctuations (analysis):
+    return len([x for x in analysis.case_tokens() if x.isalpha()])
+
+def quantify(analysis):
+    count = 0
+    pos_tags = nltk.pos_tag(analysis.case_tokens())
+    
+    for token in pos_tags:
+        if (token[1] == "NNP"):
+            count += 1
+    
+    return { "mean_multiple_naming": count / float(tokens_without_punctuations(analysis)) }
