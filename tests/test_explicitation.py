3567b6ae50634af3517ec39c36b93005ba036b74
Ohad Lutzky <ohad@lutzky.net>
2013-02-25 18:57:01 +0200

    Re-implemented Mean Multiple Naming
    Fixed tests, merged into tests/test_explication.py
    
    Closes #14

--- tests/test_explicitation.py
+++ tests/test_explicitation.py
@@ -7,6 +7,7 @@ Created on Feb 23, 2013
 import unittest
 from translationese import explicit_naming
 from translationese import single_naming
+from translationese import mean_multiple_naming
 from translationese import Analysis
 
 class TestExplicitation(unittest.TestCase):
@@ -19,6 +20,16 @@ class TestExplicitation(unittest.TestCase):
         a = Analysis("Jim, George and Bob are my friends. John Doe is not.")
         result = single_naming.quantify(a)
         self.assertAlmostEqual(3.0 / 14.0, result["single_naming"])
+
+    def testMeanMultipleNaming(self):
+        a = Analysis("John Joseph Doe easily defeated Bob Robertson.")
+        result = mean_multiple_naming.quantify(a)
+        self.assertAlmostEqual(2.5, result["mean_multiple_naming"])
+
+        a = Analysis("Dr James Brown, specialist in physics, visited in " \
+                     "Haifa University.")
+        result = mean_multiple_naming.quantify(a)
+        self.assertAlmostEqual(2.5, result["mean_multiple_naming"])
 
 if __name__ == "__main__":
     unittest.main()
--- translationese/mean_multiple_naming.py
+++ translationese/mean_multiple_naming.py
@@ -6,23 +6,28 @@ Origin: On the Features of Translationese, VV, NO & SW
 """
 from nltk.tag import pos_tag
 
-__author__ = "Gal Star"
-__email__ = "gal.star3051@gmail.com"
-
 import nltk
-
-def is_proper_noun(pos):
-    return (pos == "NNP")
-
-def tokens_without_punctuations (analysis):
-    return len([x for x in analysis.case_tokens() if x.isalpha()])
+from translationese import is_proper_noun
 
 def quantify(analysis):
-    count = 0
+    num_proper_noun_runs = 0
+    num_proper_noun_tokens = 0
+    currently_in_run = False
+
     pos_tags = nltk.pos_tag(analysis.case_tokens())
-    
+
     for token in pos_tags:
-        if (token[1] == "NNP"):
-            count += 1
+        if is_proper_noun(token):
+            currently_in_run = True
+            num_proper_noun_tokens += 1
+        elif currently_in_run:
+            # Run just ended
+            currently_in_run = False
+            num_proper_noun_runs += 1
+
+    if num_proper_noun_runs == 0:
+        result = 0
+    else:
+        result = float(num_proper_noun_tokens) / num_proper_noun_runs
     
-    return { "mean_multiple_naming": count / float(tokens_without_punctuations(analysis)) }
+    return { "mean_multiple_naming": result }
