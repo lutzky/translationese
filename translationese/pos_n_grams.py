f45a1e1e6d2e3994d3225c4c2594c998869a1811
Ohad Lutzky <ohad@lutzky.net>
2013-02-28 19:44:31 +0200

    pos_n_grams: Massive performance improvement
    analyze.py no longer requires output dictionaries to contain all
    possible keys.
    
    Again, closes #20

--- translationese/pos_n_grams.py
+++ translationese/pos_n_grams.py
@@ -3,20 +3,17 @@ Implementation of Interference hypothesis.
 Origin
 """
 
-from translationese.utils import possible_tags, set_cartesian_power, \
-    output_filter_ngram
+from translationese.utils import output_filter_ngram
 from nltk.util import ingrams
 
 def quantify_variant(analysis, variant):
     n = variant + 1
 
-    d = { output_filter_ngram(ngram): 0
-          for ngram in set_cartesian_power(possible_tags, n)}
+    d = {}
 
     all_pos_tags = [ pos for (token, pos) in analysis.pos_tags() ]
-    print set(all_pos_tags)
 
     for ngram in ingrams(all_pos_tags, n):
-        d[output_filter_ngram(ngram)] += 1
+        d[ngram] = d.get(ngram, 0) + 1
 
-    return d
+    return {output_filter_ngram(k): v for (k, v) in d.items()}
