059b558273364cdc1dff715459df3fa3568bb255
Ohad Lutzky <ohad@lutzky.net>
2013-03-01 19:16:02 +0200

    Character N-grams: Improved test
    Affects #21

--- tests/test_character_n_grams.py
+++ tests/test_character_n_grams.py
@@ -11,20 +11,21 @@ from tests.util import SparseDictEqualMixin
 
 class TestCharacterNGrams(SparseDictEqualMixin, unittest.TestCase):
     def setUp(self):
-        self.analysis = Analysis("Hello, world!")
+        self.analysis = Analysis("Hello, a world!")
         self.quantify = character_n_grams.quantify_variant
 
     def testUnigrams(self):
         self.variant = 0
         self.result = self.quantify(self.analysis, self.variant)
         self.expected = {
-                "h": 1 / 13.0,
-                "e": 1 / 13.0,
-                "l": 3 / 13.0,
-                "o": 2 / 13.0,
-                "w": 1 / 13.0,
-                "r": 1 / 13.0,
-                "d": 1 / 13.0,
+                "a": 1 / 15.0,
+                "h": 1 / 15.0,
+                "e": 1 / 15.0,
+                "l": 3 / 15.0,
+                "o": 2 / 15.0,
+                "w": 1 / 15.0,
+                "r": 1 / 15.0,
+                "d": 1 / 15.0,
                 }
 
         self.assertSparseDictEqual(self.expected, self.result)
@@ -33,18 +34,20 @@ class TestCharacterNGrams(SparseDictEqualMixin, unittest.TestCase):
         self.variant = 1
         self.result = self.quantify(self.analysis, self.variant)
         self.expected = {
-                "<h": 1 / 13.0,
-                "he": 1 / 13.0,
-                "el": 1 / 13.0,
-                "ll": 1 / 13.0,
-                "lo": 1 / 13.0,
-                "o>": 1 / 13.0,
-                "<w": 1 / 13.0,
-                "wo": 1 / 13.0,
-                "or": 1 / 13.0,
-                "rl": 1 / 13.0,
-                "ld": 1 / 13.0,
-                "d>": 1 / 13.0,
+                "<h": 1 / 15.0,
+                "he": 1 / 15.0,
+                "el": 1 / 15.0,
+                "ll": 1 / 15.0,
+                "lo": 1 / 15.0,
+                "o>": 1 / 15.0,
+                "<a": 1 / 15.0,
+                "a>": 1 / 15.0,
+                "<w": 1 / 15.0,
+                "wo": 1 / 15.0,
+                "or": 1 / 15.0,
+                "rl": 1 / 15.0,
+                "ld": 1 / 15.0,
+                "d>": 1 / 15.0,
                 }
 
         self.assertSparseDictEqual(self.expected, self.result)
